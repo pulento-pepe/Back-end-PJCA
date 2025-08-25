e7e477c436e93d186107f60f9ddd251387969f6f
Laboratorio Computación N° 9 <claseslab9@inacap.cl>
2025-08-25 18:22:19 -0400

    clase 2

--- monitoreo/dispositivos/models.py
+++ monitoreo/dispositivos/models.py
@@ -1,3 +1,9 @@
-from django.db import models
-
 # Create your models here.
+from django.db import models
+class Dispositivo(models.Model):
+    nombre = models.CharField(max_length=100)
+    consumo = models.IntegerField()
+    estado = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nombre
