2082173914f466767dcedaa0a9534e3dcee1b3dc
pulento-pepe <pablo.carvajal33@inacapmail.cl>
2025-08-25 16:10:20 -0400

    correcciones y tarea

--- monitoreo/dispositivos/views.py
+++ monitoreo/dispositivos/views.py
@@ -3,4 +3,20 @@ from django.http import HttpResponse
 
 # Create your views here.
 def inicio(request):
-    return HttpResponse("holae")
+    contexto = {"nombre": "pablo"}
+    return render(request, "dispositivos/inicio.html", contexto)
+
+def panel_dispositivos(request):
+    dispositivos =[
+        {"nombre": "sensor temperatura", "consumo":50},
+        {"nombre": "medidor solar", "consumo":120},
+        {"nombre": "sensor movimiento", "consumo":30},
+        {"nombre": "calefactor", "consumo":200},
+    ]
+
+    consumo_maximo = 100
+
+    return render(request, "dispositivos/panel.html",
+    {"dispositivos": dispositivos,
+    "consumo_maximo": consumo_maximo
+    })
--- monitoreo/monitoreo/urls.py
+++ monitoreo/monitoreo/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import path
 
 from dispositivos.views import inicio
+from dispositivos.views import panel_dispositivos
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',inicio),
+    path('panel',panel_dispositivos),
 ]
